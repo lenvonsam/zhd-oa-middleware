6744c0412d54e3121a102156e364bc379eec318c
li guang <601623863@qq.com>
2019-10-14 16:19:58 +0800

    表名不一致問題

--- src/main/java/zhd/oa/middleware/mapper/ExpenseReimburseMapper.java
+++ src/main/java/zhd/oa/middleware/mapper/ExpenseReimburseMapper.java
@@ -13,8 +13,8 @@ public interface ExpenseReimburseMapper {
 	 * 查询创建费用报销流程所需的主表数据 
 	 */
 	@Select(" select t1.applicant_ userid,t1.applydate_ applyDate,t1.belongdept_ belongDeptId,t1.payment_ paymethodId,t1.remk_ remk, "
-			+ " sum(t1.domoney) domoney from formtable_main_501 t "
-			+ " left join formtable_main_501_dt1 t1 on t.id = t1.mainid "
+			+ " sum(t1.domoney) domoney from formtable_main_504 t "
+			+ " left join formtable_main_504_dt1 t1 on t.id = t1.mainid "
 //			+ " where t.requestid = #{requestid} "
 			+ " where t.reqid = #{requestid} "
 			+ " group by t1.applicant_ ,t1.applydate_ ,t1.belongdept_ ,t1.payment_ ,t1.remk_ ")
@@ -24,8 +24,8 @@ public interface ExpenseReimburseMapper {
 	 * 根据主表的数据获取明细表数据
 	 */
 	@Select(" select t1.dodt expenseType, t1.domoney expenseMoney,t1.dodate expenseDate,t1.doremk expenseDt "
-			+ " from formtable_main_501 t "
-			+ " left join formtable_main_501_dt1 t1 on t.id = t1.mainid "
+			+ " from formtable_main_504 t "
+			+ " left join formtable_main_504_dt1 t1 on t.id = t1.mainid "
 //			+ " where t.requestid = #{requestid} "
 			+ " where t.reqid = #{requestid} "
 			+ " and t1.applicant_ = #{applicant} "
