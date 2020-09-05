af2dbfd273a587065a3548286a4a24f523084572
li guang <601623863@qq.com>
2020-09-05 16:51:53 +0800

    更新调车回调接口的参数名 和部分参数的空校验

--- src/main/java/zhd/oa/middleware/service/LogisticsWorkService.java
+++ src/main/java/zhd/oa/middleware/service/LogisticsWorkService.java
@@ -43,7 +43,7 @@ public class LogisticsWorkService extends BaseService {
                 msg += "流程已归档,或是已被删除!";
             }
 
-            JSONArray detailJSONArray = jsonObject.getJSONArray("detail");
+            JSONArray detailJSONArray = jsonObject.getJSONArray("details");
             if(null == detailJSONArray){
                 msg += "参数异常!";
             }
@@ -54,19 +54,19 @@ public class LogisticsWorkService extends BaseService {
                     msgDt = "";
 
                     JSONObject detailJSONObject = (JSONObject) detailJSONArray.get(i);
-                    String closeCom = detailJSONObject.getString("closeCom");// 0:交投智联云  1:线下结算
-                    String tranCom = detailJSONObject.getString("tranCom"); //运输单位名称
-                    String driver = detailJSONObject.getString("driver"); //司机姓名
-                    String phone = detailJSONObject.getString("phone");// 手机号
-                    String carNo = detailJSONObject.getString("carNo");// 车牌号
-                    String freightMode = detailJSONObject.getString("freightMode");// 0:不含税 1:含税
-                    String price = detailJSONObject.getString("price");// 两位小数
-                    String unit = detailJSONObject.getString("unit");// 0:/车 1:/吨
-                    String weight = detailJSONObject.getString("weight");//三位小数
-                    String money = detailJSONObject.getString("money");// 两位小数
+                    String closeCom = detailJSONObject.getString("settleCompany");// 0:交投智联云  1:线下结算
+                    String tranCom = detailJSONObject.getString("transportCompany"); //运输单位名称
+                    String driver = detailJSONObject.getString("driverName"); //司机姓名
+                    String phone = detailJSONObject.getString("driverPhone");// 手机号
+                    String carNo = detailJSONObject.getString("transportCarNo");// 车牌号
+                    String freightMode = detailJSONObject.getString("freightType");// 0:不含税 1:含税
+                    String price = detailJSONObject.getString("freightPrice");// 两位小数
+                    String unit = detailJSONObject.getString("freightUnit");// 0:/车 1:/吨
+                    String weight = detailJSONObject.getString("transportWeight");//三位小数
+                    String money = detailJSONObject.getString("freightAmount");// 两位小数
                     String carMax = detailJSONObject.getString("carMax");//
                     String carLength = detailJSONObject.getString("carLength");//
-                    String remk = detailJSONObject.getString("remk");//
+                    String remk = detailJSONObject.getString("remark");//
 
                     String comCode = logisticsWorkMapper.checkComname(tranCom);
                     String driverCode = logisticsWorkMapper.checkDriver(driver);
@@ -79,7 +79,7 @@ public class LogisticsWorkService extends BaseService {
                         }
                     }
 
-                    if(!"0".equals(closeCom) && !"1".equals(closeCom)){
+                    if(null==comCode || (!"0".equals(closeCom) && !"1".equals(closeCom))){
                         msgDt += "结算单位不正确!";
                     }
                     if(null==comCode || "".equals(comCode)){
@@ -88,25 +88,25 @@ public class LogisticsWorkService extends BaseService {
                     if(null==driverCode || "".equals(driverCode)){
                         msgDt += "司机不在系统中!";
                     }
-                    if(!phone.matches("^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(17[013678])|(18[0,5-9]))\\d{8}$")){
+                    if(null == phone || !phone.matches("^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(17[013678])|(18[0,5-9]))\\d{8}$")){
                         msgDt += "手机号不正确!";
                     }
-                    if(!car){
+                    if(null == freightMode || !car){
                         msgDt += "车牌号不正确!";
                     }
-                    if(!"0".equals(freightMode) && !"1".equals(freightMode)){
+                    if(null == freightMode || (!"0".equals(freightMode) && !"1".equals(freightMode))){
                         msgDt += "运费类型不正确!";
                     }
-                    if(!price.matches("^(([1-9][0-9]{0,14})|([0]{1})|(([0]\\.\\d{1,2}|[1-9][0-9]{0,14}\\.\\d{1,2})))$")){
+                    if(null == price || !price.matches("^(([1-9][0-9]{0,14})|([0]{1})|(([0]\\.\\d{1,2}|[1-9][0-9]{0,14}\\.\\d{1,2})))$")){
                         msgDt += "单价格式不正确!";
                     }
-                    if(!"0".equals(unit) && !"1".equals(unit)){
+                    if(null == unit || !"0".equals(unit) && !"1".equals(unit)){
                         msgDt += "运费单位不正确!";
                     }
-                    if(!weight.matches("^[0-9]+\\.{0,1}[0-9]{0,3}$")){
+                    if(null== weight || !weight.matches("^[0-9]+\\.{0,1}[0-9]{0,3}$")){
                         msgDt += "吨位格式不正确!";
                     }
-                    if(!money.matches("^(([1-9][0-9]{0,14})|([0]{1})|(([0]\\.\\d{1,2}|[1-9][0-9]{0,14}\\.\\d{1,2})))$")){
+                    if(null== money || !money.matches("^(([1-9][0-9]{0,14})|([0]{1})|(([0]\\.\\d{1,2}|[1-9][0-9]{0,14}\\.\\d{1,2})))$")){
                         msgDt += "金额格式不正确!";
                     }
                     if(null != carMax && carMax.length()>=20){
